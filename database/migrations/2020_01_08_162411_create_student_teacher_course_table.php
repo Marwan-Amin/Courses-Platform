e748f81ff6fe8f2675896d9e4623efa6ef8c8079
Amr Samy <amr.saami@gmail.com>
2020-01-08 19:54:44 +0200

    make migrations

---
+++ database/migrations/2020_01_08_162411_create_student_teacher_course_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentTeacherCourseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_teacher_course', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedInteger('student_id');
+            $table->foreign('student_id')->references('id')->on('students');
+
+            $table->string('teacher_id');
+            $table->string('teacher_id')->references('Nid')->on('users');
+
+            $table->unsignedInteger('course_id');
+            $table->foreign('course_id')->references('id')->on('courses');
+      
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_teacher_course');
+    }
+}
---
+++ database/migrations/2020_01_08_165725_create_teacher_supporter_course_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeacherSupporterCourseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teacher_supporter_course', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            
+            $table->string('teacher_id');
+            $table->foreign('teacher_id')->references('Nid')->on('users');
+
+            $table->string('supporter_id');
+            $table->foreign('supporter_id')->references('Nid')->on('users');
+
+            $table->unsignedInteger('course_id');
+            $table->foreign('course_id')->references('id')->on('courses');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teacher_supporter_course');
+    }
+}
