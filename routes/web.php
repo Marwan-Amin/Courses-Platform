c80ceb060c16f64ac4ac35ae77598ce7a03b2e24
amr samy <amr.saami@gmail.com>
2020-01-13 22:56:13 +0200

    fix conflict

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Mail;
 
 
 
-Auth::routes();
+Auth::routes(['verify'=>true]);
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home')->middleware('auth');
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 
